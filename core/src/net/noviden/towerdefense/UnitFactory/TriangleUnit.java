e68c1dc2c72707cdcaa72fd902d749537621b6da
russon77 <russon77@gmail.com>
2015-05-25 20:56:32 -0400

    forgot license header

--- core/src/net/noviden/towerdefense/UnitFactory/TriangleUnit.java
+++ core/src/net/noviden/towerdefense/UnitFactory/TriangleUnit.java
@@ -1,8 +1,25 @@
+/**
+ TowerDefense : Infinite Tower Defense Game With User Created Maps
+ Copyright (C) 2015 Tristan Kernan
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.noviden.towerdefense.UnitFactory;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.MathUtils;
 
 import net.noviden.towerdefense.Path;
 import net.noviden.towerdefense.Point;
